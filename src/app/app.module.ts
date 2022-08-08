1c098a2bc74b3039b1ce16a0677e93a1287bdb1e
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-08-08 16:56:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LoanComponent } from './loan/loan.component';
+import { EduComponent } from './loan/edu/edu.component';
+import { HomeComponent } from './loan/home/home.component';
+import { PerComponent } from './loan/per/per.component';
+import { GoldComponent } from './loan/gold/gold.component';
+import { XYZComponent } from './loan/xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoanComponent,
+    EduComponent,
+    HomeComponent,
+    PerComponent,
+    GoldComponent,
+    XYZComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/loan/xyz/xyz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css']
+})
+export class XYZComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
